e36f9c8c0fb479ffbeca15bea5625e3c2f97e78c
Oliver Roßhoff <oliver.rosshoff@gmail.com>
2022-05-30 15:41:28 +0200

    added: sum app

--- sum-app.js
+++ sum-app.js
@@ -1,3 +1,28 @@
 class SumApp {
+  constructor() {
+    // constructor wird beim Erstellen ausgeführt
+    this.numbers = [];
+  }
+
+  addNumber(n) {
+    this.numbers.push(n);
+  }
+
+  getSum() {
+    let sum = 0;
+    console.log(this.numbers.length);
+
+    for (let i = 0; i < this.numbers.length; i++) {
+      sum += this.numbers[i];
+    }
+    return sum;
+  }
+
+  reset() {
+    this.numbers = [];
+  }
+
   // Add you solution here
 }
+
+let sumApp = new SumApp();
